3a41a2cff29e5b7ea2738f44da9e355183ece421
smileperez <aa.efimenko@yandex.ru>
2023-03-28 21:34:24 +0300

    first commit

---
+++ src/components/Hoc.js
@@ -0,0 +1,17 @@
+// import React from "react";
+// import Popular from '../App';
+// import New from '../App';
+
+// function HOC(Component) {
+//     function Wrapper(props) {
+//         if (props.views > 1000) {
+//             return <Popular><Component {...props}/></Popular>
+//         } else if (props.views < 100) {
+//             return <New><Component {...props}/></New>
+//         }
+//         return <Component {...props}/>;
+//     }
+//     return Wrapper;
+// }
+
+// export default HOC;
